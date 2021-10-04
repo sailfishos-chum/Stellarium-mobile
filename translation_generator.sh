28c221d9b6764cc416249d0a44367b59f88a74e3
Lionel Duboeuf <lduboeuf@ouvaton.org>
2021-10-04 09:20:28 +0200

    bump version to 0.4.0

--- translation_generator.sh
+++ translation_generator.sh
@@ -9,10 +9,12 @@ mkdir -p mobileData/translations/stellarium-skycultures
 for lang in $LANGS; do
     lconvert -i po/stellarium/$lang.po \
              -o mobileData/translations/stellarium/$lang.qm
+    echo "converted po/stellarium/$lang.po to mobileData/translations/stellarium/$lang.qm"
 done
 
 LANGS=`ls po/stellarium-skycultures/*.po | cut -d "." -f 1 | cut -d "/" -f 3`
 for lang in $LANGS; do
   lconvert -i po/stellarium-skycultures/$lang.po \
                -o mobileData/translations/stellarium-skycultures/$lang.qm
+  echo "po/stellarium-skycultures/$lang.po to mobileData/translations/stellarium-skycultures/$lang.qm"
 done
